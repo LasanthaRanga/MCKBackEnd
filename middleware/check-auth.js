34ec908d67202440b58c58c4fec5fabbd4020d31
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-08-06 10:58:48 +0530

    update envirnment variable

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
     console.log(req.headers);
     try {  
         const token = req.headers.authorization.split(" ")[1];
-        const decode = jwt.verify(token, process.env.JWT_KEY)
+        const decode = jwt.verify(token, "secret")
         req.userData = decode;
         next();
     } catch (error) {

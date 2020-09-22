e1802603b79c6c2a706f34cddf301a13e59703e8
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-09-22 11:43:54 +0530

    Update For MCK Server

--- util/conn.js
+++ util/conn.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
+    // host: process.env.host,
+    // user: process.env.user,
+    // password: process.env.password,
+    // database: process.env.database
 
-    // host: 'localhost',
-    // user: 'root',
-    // password: '@Mck_#321',
-    // database: 'atd'
+    host: 'localhost',
+    user: 'root',
+    password: '@Mck_#321',
+    database: 'atd'
 
     // host: 'localhost',
     // user: 'root',
